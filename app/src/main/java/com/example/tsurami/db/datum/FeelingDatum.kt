427504b7b0c18749b1186e03f30b17e2efb96af3
tobutosho <tobutosho@gmail.com>
2021-09-17 14:59:58 +0900

    表示関係の調整とダークモード対応。

--- app/src/main/java/com/example/tsurami/db/datum/FeelingDatum.kt
+++ app/src/main/java/com/example/tsurami/db/datum/FeelingDatum.kt
@@ -1,10 +1,12 @@
 package com.example.tsurami.db.datum
 
+import android.annotation.SuppressLint
 import androidx.room.Embedded
 import androidx.room.Relation
 import com.example.tsurami.db.entity.Comment
 import com.example.tsurami.db.entity.Feeling
 import com.example.tsurami.db.entity.Location
+import java.text.SimpleDateFormat
 
 data class FeelingDatum(
     @Embedded
@@ -21,4 +23,27 @@ data class FeelingDatum(
         entityColumn = "feeling_id"
     )
     val comment: Comment?
-)
+) {
+    @SuppressLint("SimpleDateFormat")
+    override fun toString(): String {
+        val sdf = SimpleDateFormat("yyyy-MM-dd {HH:mm:ss}")
+        return ":<FeelingDatum>\n" +
+                "  [:feeling :location :comment]\n" +
+                "  :<Feeling>\n" +
+                "    [:datetime :a :b]\n" +
+                "    :${sdf.format(feeling.createDate)}\n" +
+                "    :${feeling.mentalParamA}\n" +
+                "    :${feeling.mentalParamB}\n" +
+                "  ;\n" +
+                "  :<Location>\n" +
+                "    [:latitude :longitude]\n" +
+                "    :${location?.latitude}\n" +
+                "    :${location?.longitude}\n" +
+                "  ;\n" +
+                "  :<Comment>\n" +
+                "    [:description]\n" +
+                "    :${comment?.content}\n" +
+                "  ;\n" +
+                ";"
+    }
+}
